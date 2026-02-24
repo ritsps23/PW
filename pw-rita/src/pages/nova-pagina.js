9f637585185331331ec2865431e6acc6d76f6c0d
ritsps23 <ritapscosta@ua.pt>
2026-02-24 21:37:31 +0000

    Add nova-pagina.js

---
+++ pw-rita/src/pages/nova-pagina.js
@@ -0,0 +1,8 @@
+export default function NovaPagina() {
+  return (
+    <div>
+      <h1>Bem-vinda à nova página!</h1>
+      <p>Esta é a minha primeira página criada na branch feature-nova-pagina.</p>
+    </div>
+  )
+}
