fda8c8ea03160611eb217aa3627d6727612f6ab2
ritsps23 <ritapscosta@ua.pt>
2026-03-15 18:55:00 +0000

    test pipeline

--- pw-rita/src/pages/nova-pagina.js
+++ pw-rita/src/pages/nova-pagina.js
@@ -2,7 +2,9 @@ export default function NovaPagina() {
   return (
     <div>
       <h1>Bem-vinda à nova página!</h1>
-      <p>Esta é a minha primeira página criada na branch feature-nova-pagina.</p>
+      <p>
+        Esta é a minha primeira página criada na branch feature-nova-pagina.
+      </p>
     </div>
-  )
+  );
 }
