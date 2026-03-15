0553feaa23197af8adccd733ecc519fca007d145
ritsps23 <ritapscosta@ua.pt>
2026-03-15 18:57:16 +0000

    fix prettier

--- pw-rita/next.config.js
+++ pw-rita/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  output: 'export'
-}
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
